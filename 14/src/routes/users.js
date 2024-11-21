4a74c5c23794e6cdd4240aed3ede8be3d7e5fb4b
CheLaLuna <daniil20040815@gmail.com>
2024-11-21 11:28:17 +0300

    1

--- 14/src/routes/users.js
+++ 14/src/routes/users.js
@@ -8,6 +8,29 @@ export default (app) => {
   app.get("/users/new", (req, res) => res.view("src/views/users/new"));
 
   // BEGIN (write your solution here)
+  app.post("/users", (req, res) => {
+    const { firstName, lastName, email } = req.body;
 
+    const id = generateId();
+    const token = generateToken();
+    const newUser = { id, firstName, lastName, email, token };
+
+    users.push(newUser);
+
+    res.setCookie('token', token, { httpOnly: true });
+
+    res.redirect(/users/`${id}`);
+  });
+
+  app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const user = users.find(u => u.id === userId);
+
+    if (user && req.cookies.token === user.token) {
+      res.view("src/views/users/show", { user });
+    } else {
+      res.send("User not found");
+    }
+  });
   // END
 };
--- 3/src/server.js
+++ 3/src/server.js
@@ -8,7 +8,19 @@ export default () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
+  app.get('/users', async (request) => {
+    const { page = 1, per = 5 } = request.query;
 
+    const pageNumber = parseInt(page, 10);
+    const perPage = parseInt(per, 10);
+
+    const startIndex = (pageNumber - 1) * perPage;
+    const endIndex = startIndex + perPage;
+
+    const paginatedUsers = users.slice(startIndex, endIndex);
+
+    return paginatedUsers;
+  });
   // END
 
   return app;
